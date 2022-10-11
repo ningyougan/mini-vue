7bed7d8d54d6e1c5843933d0df0a28fe225557f4
Polaris <54269504@qq.com>
2022-10-11 20:03:25 +0800

    feat: readonly

---
+++ src/reactivity/baseHanmders.ts
@@ -0,0 +1,36 @@
+import { track, trigger } from "./effect";
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  };
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+
+    // 触发依赖
+    trigger(target, key);
+    return res;
+  };
+}
+
+export const mutableHandlers = {
+  get: createGetter(),
+  set: createSetter(),
+};
+
+export const readonlyHandlers = {
+  get: createGetter(true),
+
+  set(target, key, values) {
+    console.warn(`warn: key:${key} is readonly!`);
+    return true;
+  },
+};
