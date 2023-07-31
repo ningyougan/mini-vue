02392de249014ead624da174490531835ef8438c
Polaris <54269504@qq.com>
2023-07-31 23:18:26 +0800

    feat: diff(1)

--- exmaple/patchChildren/App.js
+++ exmaple/patchChildren/App.js
@@ -3,6 +3,7 @@ import { h } from "../../lib/mini-vue.esm.js";
 import ArrayToText from "./ArrayToText.js";
 import TextToText from "./TextToText.js";
 import TextToArray from "./TextToArray.js";
+import ArrayToArray from "./ArrayToArray.js";
 
 export default {
   name: "App",
@@ -11,9 +12,9 @@ export default {
     return h("div", { tId: 1 }, [
       h("p", {}, "主页"),
       // h(ArrayToText),
-      h(TextToText),
+      // h(TextToText),
       // h(TextToArray),
-      // h(ArrayToArray),
+      h(ArrayToArray),
     ]);
   },
 };
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -17,10 +17,10 @@ export function createRenderer(options) {
   function render(vnode, container) {
     // patch
     // processElement();
-    patch(null, vnode, container, null);
+    patch(null, vnode, container, null, null);
   }
 
-  function patch(n1, n2, container, parentComponent) {
+  function patch(n1, n2, container, parentComponent, anchor) {
     // ShapeFlags
     // vnode -> flag
     // 判断是否为 element
@@ -30,7 +30,7 @@ export function createRenderer(options) {
 
     switch (type) {
       case Fragment:
-        processFragment(n1, n2, container, parentComponent);
+        processFragment(n1, n2, container, parentComponent, anchor);
         break;
       case Text:
         processText(n1, n2, container);
@@ -38,9 +38,9 @@ export function createRenderer(options) {
 
       default:
         if (shapeFlag & ShapeFlags.ELEMENT) {
-          processElement(n1, n2, container, parentComponent);
+          processElement(n1, n2, container, parentComponent, anchor);
         } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
-          processComponent(n1, n2, container, parentComponent);
+          processComponent(n1, n2, container, parentComponent, anchor);
         }
         break;
     }
@@ -52,18 +52,30 @@ export function createRenderer(options) {
     container.append(textNode);
   }
 
-  function processFragment(n1, n2: any, container: any, parentComponent) {
-    mountChildren(n2.children, container, parentComponent);
+  function processFragment(
+    n1,
+    n2: any,
+    container: any,
+    parentComponent,
+    anchor
+  ) {
+    mountChildren(n2.children, container, parentComponent, anchor);
   }
 
-  function processElement(n1, n2: any, container: any, parentComponent) {
+  function processElement(
+    n1,
+    n2: any,
+    container: any,
+    parentComponent,
+    anchor
+  ) {
     if (!n1) {
-      mountElement(n2, container, parentComponent);
+      mountElement(n2, container, parentComponent, anchor);
     } else {
-      patchElement(n1, n2, container, parentComponent);
+      patchElement(n1, n2, container, parentComponent, anchor);
     }
   }
-  function patchElement(n1, n2, container, parentComponent) {
+  function patchElement(n1, n2, container, parentComponent, anchor) {
     console.log(n1);
 
     const oldProps = n1.props || EMPTY_OBJ;
@@ -71,11 +83,11 @@ export function createRenderer(options) {
 
     const el = (n2.el = n1.el);
 
-    patchChildren(n1, n2, el, parentComponent);
+    patchChildren(n1, n2, el, parentComponent, anchor);
     patchProps(el, oldProps, newProps);
   }
 
-  function patchChildren(n1, n2, container, parentComponent) {
+  function patchChildren(n1, n2, container, parentComponent, anchor) {
     const { shapeFlag } = n2;
     const prevShapeFlag = n1.shapeFlag;
     const c1 = n1.children;
@@ -91,8 +103,77 @@ export function createRenderer(options) {
     } else {
       if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
         hostSetElementText(container, "");
-        mountChildren(c2, container, parentComponent);
+        mountChildren(c2, container, parentComponent, anchor);
+      } else {
+        // array diff array
+        patchKeyedChildren(c1, c2, container, parentComponent, anchor);
       }
+    }
+  }
+
+  function patchKeyedChildren(
+    c1,
+    c2,
+    container,
+    parentComponent,
+    parentAnchor
+  ) {
+    let l2 = c2.length;
+    let i = 0;
+    let e1 = c1.length - 1;
+    let e2 = l2 - 1;
+
+    function isSomeVNodeType(n1, n2) {
+      // type
+      // key
+      return n1.type == n2.type && n1.key == n2.key;
+    }
+
+    // 左侧
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[i];
+      const n2 = c2[i];
+      if (isSomeVNodeType(n1, n2)) {
+        patch(n1, n2, container, parentComponent, parentAnchor);
+      } else {
+        break;
+      }
+
+      i++;
+    }
+
+    // 右侧
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[e1];
+      const n2 = c2[e2];
+
+      if (isSomeVNodeType(n1, n2)) {
+        patch(n1, n2, container, parentComponent, parentAnchor);
+      } else {
+        break;
+      }
+      e1--;
+      e2--;
+    }
+
+    // 新的比老的多 创建
+    if (i > e1) {
+      if (i <= e2) {
+        const nextPos = e2 + 1;
+        const anchor = nextPos > l2 ? c2[nextPos].el : null;
+
+        while (i <= e2) {
+          patch(null, c2[i], container, parentComponent, anchor);
+          i++;
+        }
+      }
+    } else if (i > e2) {
+      while (i <= e1) {
+        hostRemove(c1[i].el);
+        i++;
+      }
+    } else {
+      // 乱序部分
     }
   }
 
@@ -124,7 +205,7 @@ export function createRenderer(options) {
     }
   }
 
-  function mountElement(vnode: any, container: any, parentComponent) {
+  function mountElement(vnode: any, container: any, parentComponent, anchor) {
     // const el = (vnode.el = document.createElement(vnode.type));
     const el = (vnode.el = hostCreateElement(vnode.type));
     const { children, shapeFlag } = vnode;
@@ -132,7 +213,7 @@ export function createRenderer(options) {
     if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
       el.textContent = children;
     } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-      mountChildren(vnode.children, el, parentComponent);
+      mountChildren(vnode.children, el, parentComponent, anchor);
     }
     const { props } = vnode;
     for (const key in props) {
@@ -141,27 +222,37 @@ export function createRenderer(options) {
       hostPatchProp(el, key, null, val);
     }
 
-    // container.append(el);
-    hostInsert(el, container);
+    hostInsert(el, container, anchor);
   }
-  function mountChildren(children, container, parentComponent) {
+  function mountChildren(children, container, parentComponent, anchor) {
     children.forEach((v) => {
-      patch(null, v, container, parentComponent);
+      patch(null, v, container, parentComponent, anchor);
     });
   }
 
-  function processComponent(n1, n2: any, container: any, parentComponent) {
-    mountComponent(n2, container, parentComponent);
+  function processComponent(
+    n1,
+    n2: any,
+    container: any,
+    parentComponent,
+    anchor
+  ) {
+    mountComponent(n2, container, parentComponent, anchor);
   }
 
-  function mountComponent(initialVnode: any, container, parentComponent) {
+  function mountComponent(
+    initialVnode: any,
+    container,
+    parentComponent,
+    anchor
+  ) {
     const instance = createComponentInstance(initialVnode, parentComponent);
 
     setupComponent(instance);
-    setupRenderEffect(instance, initialVnode, container);
+    setupRenderEffect(instance, initialVnode, container, anchor);
   }
 
-  function setupRenderEffect(instance: any, initialVnode, container) {
+  function setupRenderEffect(instance: any, initialVnode, container, anchor) {
     effect(() => {
       if (!instance.isMounted) {
         console.log("init");
@@ -171,7 +262,7 @@ export function createRenderer(options) {
 
         // vnode -> patch
         // vnode -> element -> mountelement
-        patch(null, subTree, container, instance);
+        patch(null, subTree, container, instance, anchor);
         initialVnode.el = subTree.el;
 
         instance.isMounted = true;
@@ -185,7 +276,7 @@ export function createRenderer(options) {
 
         instance.subTree = subTree;
 
-        patch(prevSubTree, subTree, container, instance);
+        patch(prevSubTree, subTree, container, instance, anchor);
       }
     });
   }
